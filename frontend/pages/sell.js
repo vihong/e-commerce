cda7660d61f4e1c30fbffef156238f10785cfc61
vihong <probamaths@gmail.com>
2020-01-25 20:17:22 +0100

    :construction: Add routing system

---
+++ frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Sell = () => {
+	return (
+		<div>
+			<p>Sell</p>
+			<Link href="/">
+				<a>Back to Home</a>
+			</Link>
+		</div>
+	);
+};
+
+export default Sell;
