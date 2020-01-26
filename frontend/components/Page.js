92cb0609906303c4f9ccdb8b764f11a3d4d1946c
vihong <probamaths@gmail.com>
2020-01-26 05:32:27 +0100

    💄 Add style to Page Component

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+const themeOuiCar = {
+	red         : '#FF0000',
+	greenOuiCar : '#008a83',
+	black       : '#393939',
+	grey        : '#3A3A3A',
+	lightgrey   : '#E1E1E1',
+	offWhite    : '#EDEDED',
+	maxWidth    : '1000px',
+	bs          : '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+};
+
+const StyledPage = styled.div`
+	background: white
+	color: ${(props) => props.theme.black};
+`;
+
+const Inner = styled.div`
+	border: 1px solid black;
+	max-width: ${(props) => props.theme.maxWidth};
+	margin: 0 auto;
+	padding: 2rem;
+`;
 
 class Page extends Component {
 	render() {
 		return (
-			<div>
-				<Meta />
-				<Header />
-				{this.props.children}
-			</div>
+			<ThemeProvider theme={themeOuiCar}>
+				<StyledPage>
+					<Meta />
+					<Header />
+					<Inner>{this.props.children}</Inner>
+				</StyledPage>
+			</ThemeProvider>
 		);
 	}
 }
