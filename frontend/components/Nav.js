ab606bf0738580b5c060a530f9e66b9b5d989517
vihong <probamaths@gmail.com>
2020-01-26 06:21:03 +0100

    :construction: Add styled Navbar

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,15 +1,25 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = (props) => {
 	return (
-		<div>
+		<NavStyles>
+			<Link href="/items">
+				<a>Items</a>
+			</Link>
 			<Link href="/sell">
 				<a>Sell</a>
 			</Link>
-			<Link href="/">
-				<a>Back to Home</a>
+			<Link href="/signup">
+				<a>Signup</a>
 			</Link>
-		</div>
+			<Link href="/orders">
+				<a>Orders</a>
+			</Link>
+			<Link href="/me">
+				<a>Account</a>
+			</Link>
+		</NavStyles>
 	);
 };
 
