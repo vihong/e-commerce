946330f59d8f306a2c3accf270bea16edbac30f2
vihong <probamaths@gmail.com>
2020-01-26 22:47:53 +0100

    :card_file_box: Yoga GraphQL set up

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const mutations = {};
+const Mutations = {
+	async createItem(parent, args, ctx, info) {
+		const item = await ctx.db.mutation.createItem(
+			{
+				data : {
+					...args
+				}
+			},
+			info
+		);
+		// console.log(item);
+		return item;
+	}
+};
 
-module.exports = mutations;
+module.exports = Mutations;
