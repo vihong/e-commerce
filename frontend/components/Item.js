63aaf07841864a64faa74dccfb277b5384bfa2ba
vihong <probamaths@gmail.com>
2020-01-27 11:02:44 +0100

    :sparkles: Add Items and Item components

---
+++ frontend/components/Item.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Title from './styles/Title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
+
+class Item extends Component {
+	static propTypes = {
+		item : PropTypes.object.isRequired
+	};
+
+	render() {
+		const { item } = this.props;
+		return (
+			<ItemStyles>
+				{item.image ? <img src={item.image} alt={item.title} /> : null}
+				<Title>
+					<Link
+						href={{
+							pathname : '/item',
+							query    : { id: item.id }
+						}}
+					>
+						<a>{item.title}</a>
+					</Link>
+				</Title>
+				<PriceTag>{formatMoney(item.price)}</PriceTag>
+				<p>{item.description}</p>
+				<div className="buttonList">
+					<Link
+						href={{
+							pathname : 'update',
+							query    : { id: item.id }
+						}}
+					>
+						<a>Modifier quantité &#9997;</a>
+					</Link>
+					<button>Ajouter au panier &#128722;</button>
+					<button>Retirer du panier &#10060;</button>
+				</div>
+			</ItemStyles>
+		);
+	}
+}
+
+export default Item;
