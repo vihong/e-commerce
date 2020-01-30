ce44b501d09cc92153e16502870d531c2e80ad03
vihong <probamaths@gmail.com>
2020-01-30 06:12:13 +0100

    :lipstick: Add box-shadow to slogan

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -20,6 +20,7 @@ const CallToActionStyles = styled.p`
 	transform: rotate(-3deg);
 	position: relative;
 	bottom: 2rem;
+	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 `;
 
 const Home = (props) => {
