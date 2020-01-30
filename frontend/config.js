be62abf8a9cd7584aa2bf6f5f5cbd23766ddb76d
vihong <probamaths@gmail.com>
2020-01-30 02:19:34 +0100

    :wrench: Adjust prodEndpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = `https://etape-un-database-2235524a3d.herokuapp.com/nom-de-database/prod`;
 export const perPage = 4;
