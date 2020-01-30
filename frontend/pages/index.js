f46793e3fe87dd8a38773d83fff0cfa1db5205ad
vihong <probamaths@gmail.com>
2020-01-30 05:53:24 +0100

    :lipstick: Update homepage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,24 @@
 import Link from 'next/link';
 import Items from '../components/Items';
+import styled from 'styled-components';
+
+const SloganStyles = styled.h1`
+	margin: 2rem auto 5rem;
+	text-align: center;
+	color: ${(props) => props.theme.greenOuiCar};
+	line-height: 150%;
+	span {
+		color: ${(props) => props.theme.black};
+	}
+`;
 
 const Home = (props) => {
 	return (
 		<div>
+			<SloganStyles>
+				Avec OUICAR+, <br />
+				<span>faites plus que Louer une voiture,</span> savourez la.
+			</SloganStyles>
 			<Items />
 		</div>
 	);
