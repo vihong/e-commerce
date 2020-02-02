1278c99fce153256a2797c62ff80524c5e78814b
vihong <probamaths@gmail.com>
2020-02-02 15:02:56 +0100

    :sparkles: Update Apollo Cache if item is deleted

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { ALL_ITEMS_QUERY } from './Items';
 
 const DELETE_ITEM_MUTATION = gql`
 	mutation DELETE_ITEM_MUTATION($id: ID!) {
@@ -12,11 +13,26 @@ const DELETE_ITEM_MUTATION = gql`
 
 class DeleteItem extends Component {
 	state = {};
+
+	udpateInApolloCache = (cache, dataCurrentlyInDatabase) => {
+		const dataCurrentlyInApollo = cache.readQuery({
+			query : ALL_ITEMS_QUERY
+		});
+		console.log(dataCurrentlyInApollo, dataCurrentlyInDatabase);
+		dataCurrentlyInApollo.items = dataCurrentlyInApollo.items.filter(
+			(item) => item.id !== dataCurrentlyInDatabase.data.deleteItem.id
+		);
+		cache.writeQuery({
+			query : ALL_ITEMS_QUERY,
+			data  : dataCurrentlyInApollo
+		}); // WARNING : "data" is an official database/cache keyword, don't change it.
+	};
 	render() {
 		return (
 			<Mutation
 				mutation={DELETE_ITEM_MUTATION}
 				variables={{ id: this.props.id }}
+				update={this.udpateInApolloCache} // WARNING : "update" is an official event keyword, don't change it.
 			>
 				{(deleteItem, { error }) => (
 					<button
